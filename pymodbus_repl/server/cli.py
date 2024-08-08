68cce0306e2457192a92feefdf605319939bc61c
Alex Ruddick <alex@ruddick.tech>
2024-08-08 09:04:29 -0500

    Fix type hint for ReactiveServer

--- pymodbus_repl/server/cli.py
+++ pymodbus_repl/server/cli.py
@@ -17,6 +17,9 @@ from tabulate import tabulate
 from pymodbus_repl import __VERSION__ as repl_version
 from pymodbus_repl.lib.completer import CmdCompleter
 from pymodbus_repl.lib.helper import Command, style
+from pymodbus_repl.lib.reactive import (
+    ReactiveServer,
+)
 
 
 TITLE = r"""
@@ -167,7 +170,7 @@ def print_help(command: Optional[str] = None):
         _print_help()
 
 
-def print_server_config(server: "ReactiveServer", print_server_context: bool = False, *extra) -> dict:  # noqa
+def print_server_config(server: ReactiveServer, print_server_context: bool = False, *extra) -> dict:
     """Print server config."""
     print_formatted_text()
     print_formatted_text(HTML("<u>Server Configs:</u>"))
