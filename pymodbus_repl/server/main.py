0b5bb5855df1784b62f043392ef69606048f0f80
dhoomakethu <otlasanju@gmail.com>
2024-08-10 15:35:46 +0530

    Pymodbus 3.7.x changes part 2, cherry picking changes from https://github.com/pymodbus-dev/repl/pull/18

--- pymodbus_repl/server/main.py
+++ pymodbus_repl/server/main.py
@@ -12,7 +12,7 @@ from typing import List, Optional
 
 import typer
 from pymodbus import pymodbus_apply_logging_config
-from pymodbus.framer.socket_framer import ModbusSocketFramer
+from pymodbus.transaction import ModbusSocketFramer
 from pymodbus.logging import Log
 from typing_extensions import Annotated
 
@@ -44,12 +44,11 @@ class ModbusServerTypes(str, Enum):
 class ModbusFramerTypes(str, Enum):
     """Framer types."""
 
-    # ["socket", "rtu", "tls", "ascii", "binary"]
+    # ["socket", "rtu", "tls", "ascii"]
     socket = "socket"  # pylint: disable=invalid-name
     rtu = "rtu"  # pylint: disable=invalid-name
     tls = "tls"  # pylint: disable=invalid-name
     ascii = "ascii"  # pylint: disable=invalid-name
-    binary = "binary"  # pylint: disable=invalid-name
 
 
 def _completer(incomplete: str, valid_values: List[str]) -> List[str]:
@@ -62,13 +61,13 @@ def _completer(incomplete: str, valid_values: List[str]) -> List[str]:
 
 
 def framers(incomplete: str) -> List[str]:
-    """Return an autocompleted list of supported clouds."""
-    _framers = ["socket", "rtu", "tls", "ascii", "binary"]
+    """Return an autocompleted list of supported servers."""
+    _framers = ["socket", "rtu", "tls", "ascii"]
     return _completer(incomplete, _framers)
 
 
 def servers(incomplete: str) -> List[str]:
-    """Return an autocompleted list of supported clouds."""
+    """Return an autocompleted list of supported servers."""
     _servers = ["tcp", "serial", "tls", "udp"]
     return _completer(incomplete, _servers)
 
